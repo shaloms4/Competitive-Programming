d75a653aa74336553eb0430d10846db977867968
Shalom Habtamu <shalomhabtamum@gmail.com>
2024-07-05 14:01:04 +0300

    solved

---
+++ LeetCode/Largest Local Values in a Matrix.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def largestLocal(self, grid):
+        n =  len(grid)
+        result = [[0] * (n - 2) for _ in range (n - 2)]
+        for i in range (n - 2):
+            for j in range (n - 2):
+                for row in range (i, i + 3):
+                    for column in range (j, j + 3):
+                        result [i][j] = max(result[i][j], grid[row][column])
+        return result
+
+        
