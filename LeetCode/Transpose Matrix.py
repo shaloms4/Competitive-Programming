d99abeafc7a1380925e1b3672002ae2eaddc349b
Shalom Habtamu <shalomhabtamum@gmail.com>
2024-07-05 10:10:13 +0300

    edited solution

--- LeetCode/Transpose Matrix.py
+++ LeetCode/Transpose Matrix.py
@@ -1,9 +1,9 @@
 class Solution(object):
     def transpose(self, matrix):
-        m = len(matrix)
-        n = len(matrix[0])
-        transpose_matrix = [[0] * m for _ in range (n)]
-        for i in range(m):
-            for j in range(n):
+        row = len(matrix)
+        column = len(matrix[0])
+        transpose_matrix = [[0] * row for _ in range (column)]
+        for i in range(row):
+            for j in range(column):
                 transpose_matrix[j][i] = matrix[i][j]
         return transpose_matrix
