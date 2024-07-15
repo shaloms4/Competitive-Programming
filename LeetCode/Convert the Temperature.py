576b3b549da40e83d45d74cbffbab5aa91207e4b
Shalom Habtamu <shalomhabtamum@gmail.com>
2024-07-15 10:18:56 +0300

    edit solution

--- LeetCode/Convert the Temperature.py
+++ LeetCode/Convert the Temperature.py
@@ -1,8 +1,5 @@
 class Solution(object):
     def convertTemperature(self, celsius):
-       Kelvin = round(celsius + 273.15,5)
-       Fahrenheit = round(celsius * 1.80 + 32.00,5)
-       return [Kelvin, Fahrenheit]
-
-
-        
+        Kelvin = celsius + 273.15
+        Fahrenheit = celsius * 1.80 + 32.00
+        return [Kelvin, Fahrenheit]
