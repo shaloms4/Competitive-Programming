3e58d175edea2032efab5d8cb5108cf833009871
Shalom Habtamu <shalomhabtamum@gmail.com>
2024-07-29 14:10:09 +0300

    solved

--- LeetCode/Merge Sorted Array.py
+++ LeetCode/Merge Sorted Array.py
@@ -2,21 +2,16 @@ class Solution(object):
     def merge(self, nums1, m, nums2, n):
         p1 = m - 1
         p2 = n - 1
-        p = m + n - 1
         
-        while p1 >= 0 and p2 >= 0:
-            if nums1[p1] > nums2[p2]:
+        for p in range(m + n - 1, -1, -1):
+            if p1 < 0:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            elif p2 < 0:
+                break
+            elif nums1[p1] > nums2[p2]:
                 nums1[p] = nums1[p1]
                 p1 -= 1
             else:
                 nums1[p] = nums2[p2]
                 p2 -= 1
-            p -= 1
-        
-        while p2 >= 0:
-            nums1[p] = nums2[p2]
-            p2 -= 1
-            p -= 1
-
-        
-        
