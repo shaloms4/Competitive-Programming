1732048a97e195b91104cb07799cff182bf53f1e
Shalom Habtamu <shalomhabtamum@gmail.com>
2024-07-31 13:04:32 +0300

    solved

---
+++ LeetCode/Sum of Square Numbers.py
@@ -0,0 +1,17 @@
+import math
+class Solution(object):
+    def judgeSquareSum(self, c):
+        left = 0
+        right = int(math.sqrt(c))
+        
+        while left <= right:
+            sum_of_squares = left * left + right * right
+            if sum_of_squares == c:
+                return True
+            elif sum_of_squares < c:
+                left += 1
+            else:
+                right -= 1
+        
+        return False
+
