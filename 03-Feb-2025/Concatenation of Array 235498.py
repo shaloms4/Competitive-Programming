d6e898699449bea2acc6433df213973807b9a59e
Shalom Habtamu <150407472+shaloms4@users.noreply.github.com>
2025-02-03 11:28:22 +0300

    Created 'Concatenation of Array' by A2SV Hub

---
+++ 03-Feb-2025/Concatenation of Array 235498.py
@@ -0,0 +1,17 @@
+# Problem: Concatenation of Array - https://leetcode.com/problems/concatenation-of-array/description/
+
+class Solution:
+    def getConcatenation(self, nums):
+        n = len(nums)
+        ans = [0] * (2 * n)
+        
+        for i in range(n):
+            ans[i] = nums[i]
+            ans[i + n] = nums[i]
+        
+        return ans
+
+param_1=[1,2,1]
+result=Solution().getConcatenation(param_1)
+print(result)
+
