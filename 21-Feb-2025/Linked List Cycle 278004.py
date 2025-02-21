1d4f2a4d0b7d1a47ae06f83094a7a637d27413d6
Shalom Habtamu <150407472+shaloms4@users.noreply.github.com>
2025-02-21 10:08:46 +0300

    Created 'Linked List Cycle' by A2SV Hub

---
+++ 21-Feb-2025/Linked List Cycle 278004.py
@@ -0,0 +1,19 @@
+# Problem: Linked List Cycle - https://leetcode.com/problems/linked-list-cycle/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        cur = head
+        check = set()
+        while cur:
+            if cur in check:
+                return True
+            check.add(cur)
+            cur = cur.next
+        return False
+        
