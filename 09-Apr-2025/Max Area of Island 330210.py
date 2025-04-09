84cdc5212a9865f3b93b527127fd622b98cf559c
Shalom Habtamu <shalomhabtamum@gmail.com>
2025-04-09 22:38:33 +0300

    Created 'Max Area of Island' by A2SV Hub

---
+++ 09-Apr-2025/Max Area of Island 330210.py
@@ -0,0 +1,31 @@
+# Problem: Max Area of Island - https://leetcode.com/problems/max-area-of-island/
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        if not grid:
+            return 0
+
+        max_area = 0
+        rows, cols = len(grid), len(grid[0])
+
+        def dfs(r, c):
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == 0:
+                return 0
+            
+            grid[r][c] = 0
+            area = 1  
+
+            area += dfs(r + 1, c)  
+            area += dfs(r - 1, c)  
+            area += dfs(r, c + 1)  
+            area += dfs(r, c - 1)  
+
+            return area
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:  
+                    current_area = dfs(r, c)
+                    max_area = max(max_area, current_area)
+
+        return max_area
